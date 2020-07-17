7028d6ac40725b31ed512aca3fb0bf14d57ebc6a
Daniil <me@dan0102dan.ru>
2020-07-17 14:04:18 +0300

    little changes

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -6,8 +6,6 @@ export default class Footer extends React.Component {
     return (
       <Layout.Footer style={{textAlign: 'center', background: '#57a76b'}}>
         Made with <span role="img" aria-label="RedHeart">❤️</span> <a style={{color: '#5d3939'}} href="https://dan0102dan.ru">dan0102dan</a>
-        <span> and </span>
-        <a style={{color: '#49395d'}} href="https://github.com/facebook/create-react-app">creat-react-app</a>
       </Layout.Footer>
     );
   }
