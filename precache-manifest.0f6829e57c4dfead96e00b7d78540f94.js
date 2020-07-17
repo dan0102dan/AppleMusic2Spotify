3af15a2be549f99f050b9a7720c111c1e5466d7a
Daniil <me@dan0102dan.ru>
2020-07-17 10:34:41 +0300

    Updates

---
+++ precache-manifest.0f6829e57c4dfead96e00b7d78540f94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24028ffa849e8800216153e334bb859b",
+    "url": "/AppleMusic2Spotify/index.html"
+  },
+  {
+    "revision": "4355c701924a54fcd876",
+    "url": "/AppleMusic2Spotify/static/css/2.a7bad2f4.chunk.css"
+  },
+  {
+    "revision": "d4f1b916a728a52b3899",
+    "url": "/AppleMusic2Spotify/static/css/main.130932e9.chunk.css"
+  },
+  {
+    "revision": "4355c701924a54fcd876",
+    "url": "/AppleMusic2Spotify/static/js/2.932e811e.chunk.js"
+  },
+  {
+    "revision": "a149db398379728c84fc066dce284882",
+    "url": "/AppleMusic2Spotify/static/js/2.932e811e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f1b916a728a52b3899",
+    "url": "/AppleMusic2Spotify/static/js/main.351918d0.chunk.js"
+  },
+  {
+    "revision": "80b5d49d999d62998ea9",
+    "url": "/AppleMusic2Spotify/static/js/runtime-main.f74ae169.js"
+  }
+]);
