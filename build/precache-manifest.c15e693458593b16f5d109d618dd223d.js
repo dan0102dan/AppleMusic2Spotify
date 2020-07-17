d8db9aefeea50d9d3947457d9195bec0e2085b82
Daniil <me@dan0102dan.ru>
2020-07-17 14:02:01 +0300

    test

---
+++ build/precache-manifest.c15e693458593b16f5d109d618dd223d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679027e28c5ce0d30561afc5baf42408",
+    "url": "/applemusic2spotify/index.html"
+  },
+  {
+    "revision": "961ee9c7c04ae9fbc7b3",
+    "url": "/applemusic2spotify/static/css/2.d88ab335.chunk.css"
+  },
+  {
+    "revision": "0f7c3168c1a89bd4f599",
+    "url": "/applemusic2spotify/static/css/main.686534b3.chunk.css"
+  },
+  {
+    "revision": "961ee9c7c04ae9fbc7b3",
+    "url": "/applemusic2spotify/static/js/2.02c91418.chunk.js"
+  },
+  {
+    "revision": "a149db398379728c84fc066dce284882",
+    "url": "/applemusic2spotify/static/js/2.02c91418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7c3168c1a89bd4f599",
+    "url": "/applemusic2spotify/static/js/main.0e18b4e2.chunk.js"
+  },
+  {
+    "revision": "90e7b06adb8f1d63b2df",
+    "url": "/applemusic2spotify/static/js/runtime-main.de49b6fc.js"
+  }
+]);
