ab4affb6f10f3de4a6071fe0e56b5d27b92560ed
Daniil <me@dan0102dan.ru>
2020-07-18 13:34:21 +0300

    Updates

---
+++ precache-manifest.a1b8542637ae139276a96d92fd105e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74aef7a5941e77fa8e466a675edfab2",
+    "url": "/applemusic2spotify/index.html"
+  },
+  {
+    "revision": "f58e596585f961162477",
+    "url": "/applemusic2spotify/static/css/2.d88ab335.chunk.css"
+  },
+  {
+    "revision": "fe45f1710284c4174a83",
+    "url": "/applemusic2spotify/static/css/main.9ce0a29d.chunk.css"
+  },
+  {
+    "revision": "f58e596585f961162477",
+    "url": "/applemusic2spotify/static/js/2.15ea5a11.chunk.js"
+  },
+  {
+    "revision": "a149db398379728c84fc066dce284882",
+    "url": "/applemusic2spotify/static/js/2.15ea5a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe45f1710284c4174a83",
+    "url": "/applemusic2spotify/static/js/main.e9ce968f.chunk.js"
+  },
+  {
+    "revision": "90e7b06adb8f1d63b2df",
+    "url": "/applemusic2spotify/static/js/runtime-main.de49b6fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/applemusic2spotify/precache-manifest.00ebb6443b3e38619a75e4f5a43bc56e.js"
+  "/applemusic2spotify/precache-manifest.a1b8542637ae139276a96d92fd105e87.js"
 );
 
 self.addEventListener('message', (event) => {
