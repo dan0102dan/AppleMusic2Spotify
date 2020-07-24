e423800cfa364bb6aa218b675cfbbf96ffff68cc
Daniil <me@dan0102dan.ru>
2020-07-24 17:54:34 +0300

    Updates

---
+++ precache-manifest.ee9718b2898cb77eb9901c39a3129d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8f7b7117193436f0e498e5760b04c3",
+    "url": "/2spotify/index.html"
+  },
+  {
+    "revision": "8e0428ecbab1cb6dd288",
+    "url": "/2spotify/static/css/2.d88ab335.chunk.css"
+  },
+  {
+    "revision": "b60a6b5192c8bf2886dd",
+    "url": "/2spotify/static/css/main.6d20a323.chunk.css"
+  },
+  {
+    "revision": "8e0428ecbab1cb6dd288",
+    "url": "/2spotify/static/js/2.5a94548e.chunk.js"
+  },
+  {
+    "revision": "a149db398379728c84fc066dce284882",
+    "url": "/2spotify/static/js/2.5a94548e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b60a6b5192c8bf2886dd",
+    "url": "/2spotify/static/js/main.423ea7b7.chunk.js"
+  },
+  {
+    "revision": "f72b794c68019ce0a806",
+    "url": "/2spotify/static/js/runtime-main.78161879.js"
+  }
+]);
