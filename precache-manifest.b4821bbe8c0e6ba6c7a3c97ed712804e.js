cf38481e2167c3643d788a62778cf83d842927a8
Daniil <me@dan0102dan.ru>
2020-07-24 23:53:24 +0300

    Updates

---
+++ precache-manifest.b4821bbe8c0e6ba6c7a3c97ed712804e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b3a50591e98b5e794d266e8f36903f",
+    "url": "/2spotify/index.html"
+  },
+  {
+    "revision": "3b7830b23a86e98f0890",
+    "url": "/2spotify/static/css/2.d88ab335.chunk.css"
+  },
+  {
+    "revision": "019d532de3c8bd84b44c",
+    "url": "/2spotify/static/css/main.6d20a323.chunk.css"
+  },
+  {
+    "revision": "3b7830b23a86e98f0890",
+    "url": "/2spotify/static/js/2.e6e7820c.chunk.js"
+  },
+  {
+    "revision": "a149db398379728c84fc066dce284882",
+    "url": "/2spotify/static/js/2.e6e7820c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019d532de3c8bd84b44c",
+    "url": "/2spotify/static/js/main.34d9730c.chunk.js"
+  },
+  {
+    "revision": "f72b794c68019ce0a806",
+    "url": "/2spotify/static/js/runtime-main.78161879.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2spotify/precache-manifest.631ab7d11a98e24072012cdecc5c957d.js"
+  "/2spotify/precache-manifest.b4821bbe8c0e6ba6c7a3c97ed712804e.js"
 );
 
 self.addEventListener('message', (event) => {
